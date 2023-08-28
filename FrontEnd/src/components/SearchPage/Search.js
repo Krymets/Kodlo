9d8e146c1a2d369f42fd44896ea6f98ab3058003
mojoym <mojoym@gmail.com>
2023-08-28 13:01:29 +0300

    pagination result pages style added

--- FrontEnd/src/components/SearchPage/Search.js
+++ FrontEnd/src/components/SearchPage/Search.js
@@ -1,9 +1,13 @@
 import './search_page.css';
 import frame42 from './img/frame42.png';
+import link_to_left from './img/link_to_left.svg';
+import link_to_right from './img/link_to_right.svg';
 import React, { useState, useEffect } from 'react';
 import SearchResults from './search_field/SearchResults';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+
+const ITEMS_PER_PAGE = 6;
 
 export function Search () {
     const [searchResults, setSearchResults] = useState([]);
@@ -13,6 +17,7 @@ export function Search () {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const searchTerm = searchParams.get('search_field');
+    
 
     useEffect(() => {
         if (searchTerm) {
@@ -30,6 +35,18 @@ export function Search () {
         }
     }, [searchTerm]);
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalItems = searchResults.length;
+    const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+    
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const displayedResults = searchResults.slice(startIndex, endIndex);
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
+
     return (
         <div className='main_block'>
             <img className='frame-img-right' src={frame42} alt="frame" />
@@ -40,12 +57,61 @@ export function Search () {
             <div className='new-companies-main'>
                 {!error && searchResults.length > 0 ? (
                     <>
-                        <SearchResults results={searchResults} searchPerformed={searchPerformed} searchTerm={searchTerm} />
+                        <SearchResults results={searchResults} searchPerformed={searchPerformed} displayedResults={displayedResults} />
                         <br/>
                     </>
                 ) : (
                     <p className='search_result_error'>Пошук не дав результатів: компанії з іменем <span className='.search_result_error search_result_error_search_value'>{searchTerm}</span> не було виявлено на даний момент</p>
                 )}
+            </div>
+            <div className='new-companies-result_pages'>
+                {totalPages > 1 && (
+                            <div className="pagination">
+                                {currentPage > 1 && (
+                                    <button onClick={() => handlePageChange(currentPage - 1)}>
+                                        <img src={link_to_left} alt="Link to Left" />
+                                    </button>
+                                )}
+                                {currentPage > 1 && (
+                                    <>
+                                        <button onClick={() => handlePageChange(1)}>1</button>
+                                        {currentPage > 2 && <span className="ellipsis">...</span>}
+                                    </>
+                                )}
+                                {Array.from({ length: totalPages }, (_, i) => {
+                                    if (
+                                        i === 2 ||
+                                        i === totalPages ||
+                                        // i != totalPages ||
+                                        (i >= currentPage - 1 && i <= currentPage )
+                                    ) {
+                                        return (
+                                            <button
+                                                key={i}
+                                                onClick={() => handlePageChange(i + 1)}
+                                                className={currentPage === i + 1 ? 'active' : ''}
+                                            >
+                                                {i + 1}
+                                            </button>
+                                        );
+                                    }
+                                    return null;
+                                })}         
+                                {currentPage < totalPages - 1 && (
+                                    <>
+                                        {currentPage < totalPages - 1 && <span className="ellipsis">...</span>}
+                                        <button onClick={() => handlePageChange(totalPages)}>
+                                            {totalPages}
+                                        </button>
+                                    </>
+                                )}
+                                {currentPage < totalPages && (
+                                    <button onClick={() => handlePageChange(currentPage + 1)}>
+                                        <img src={link_to_right} alt="Link to Right" />
+                                    </button>
+                                )}
+                            </div>
+                        )}
             </div>
         </div>
     )
--- FrontEnd/src/components/SearchPage/search_field/companies/Companies.js
+++ FrontEnd/src/components/SearchPage/search_field/companies/Companies.js
@@ -1,18 +1,63 @@
 // import 'src/components/landing-page/companies/Companies.css'
 import './CompaniesCards.css';
-import like_logo from './like_logo.svg';
-import wish_list_checklist from './wish_list_checklist.svg';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import add_to_wish_list from './img/add_to_wish_list.svg';
+import in_wish_list from './img/in_wish_list.svg'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 const MainCompanies = ({ companyData }) => {
+    const navigate = useNavigate();
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
     
+    
+    const [isInWishList, setIsInWishList] = useState(false);
+    // const user = ...                                                                             // add here to find user id
+
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ${user.token}',                                                   // user token here
+        },
+    };
+
+    // companies profile_id
+    const data = {
+        company_pk: companyData.profile_id,
+    };
+
+    // check if company in wish list
+    axios.post('check-wish-list/', data, options)
+        .then(response => {
+            setIsInWishList(response.data.exists);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    // add company to wish list 
+    const addToWishList = () => {
+        axios.post('add-to-wish-list/', data, options)
+            .then(response => {
+                setIsInWishList(true);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    const loginPage = () => {
+        navigate(`/authorization`);
+    }; 
+
     return (
         <div className="product-card">
             <div className="product-card__block">
                 <div className="product-card__image-frame">
-                    <img className="product-card__image" src={companyData.comp_banner_image} alt={companyData.comp_name} />
+                    <Link className='product-card__name-text_link' to={`/company/${companyData.profile_id}`}>
+                        <img className="product-card__image" src={companyData.comp_banner_image} alt={companyData.comp_name} />
+                        </Link>
                 </div>
                 <div className="product-card__text-block">
                     <div className="product-card__text-block__header">
@@ -29,9 +74,29 @@ const MainCompanies = ({ companyData }) => {
                                 <div className="product-card__badge-text">{currentYear - companyData.comp_year_of_foundation} років досвіду</div>
                             </div>
                         </div>
-                    </div>
+                    </div>                                                                                          
+                        {/* { !user  && (                                                                               // add user data here if user is not authorized redirect to login page
+                            <button className='product-card__buttons' onClick={() => loginPage()}>
+                                <img src={add_to_wish_list} width="25" height="25" alt="addToWhishList"/>
+                            </button>
+                        )}
+                        { user && isInWishList && (                                                                     // add user data here if user is authorized add com in wish list                                                                               
+                            <button className='product-card__buttons'>
+                                <img src={in_wish_list} width="40" height="40" alt="inWishList"/>  
+                            </button>                                                                                            
+                        )}
+                        { user && !isInWishList && (                                                                    // add user data here if user is authorized add com not in wish list      
+                            <button className='product-card__buttons' onClick={addToWishList}>                          
+                                <img src={add_to_wish_list} width="25" height="25" alt="addToWhishList"/>
+                            </button>
+                        )} */}
+
+                        {/* add check user && wish list here */}                                                       
+                        <button className='product-card__buttons' onClick={() => loginPage()}>  
+                            <img src={add_to_wish_list} width="25" height="25" alt="addToWhishList"/>  
+                        </button>
                     <div>
-                        <button className='product-card__buttons'><img src={wish_list_checklist} width="25" height="25" alt=""/></button>
+
                     </div>
                 </div>
             </div>
@@ -42,6 +107,30 @@ const MainCompanies = ({ companyData }) => {
             </div>
         </div>
 
+
+
+
+
+// { !user  && (                                                                                    // add user data here if user is not authorized redirect to login page
+// <button className='product-card__buttons' onClick={addToWishList}>
+//     <img src={add_to_wish_list} width="25" height="25" alt="addToWhishList"/>
+// </button>
+// //                        )}
+// //                        { user && isInWishList && (                                                                     // add user data here if user is authorized add com in wish list                                                                               
+// <button className='product-card__buttons'>
+//     <img src={in_wish_list} width="25" height="25" alt="inWishList"/>  
+// </button>                                                                                            
+// //                       )}
+// //                            {user && !isInWishList && (                                                                    // add user data here if user is authorized add com not in wish list      
+//     <button className='product-card__buttons' onClick={addToWishList}>                          
+//     <img src={add_to_wish_list} width="25" height="25" alt="addToWhishList"/>
+// </button>
+// //                       )}
+
+// {/* add check user && wish list here */}                                                       
+// <button className='product-card__buttons'>  
+// <img src={add_to_wish_list} width="25" height="25" alt="inWishList"/>  
+// </button> 
 
 
 
