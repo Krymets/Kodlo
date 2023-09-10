14e7522f70a8abce1466aac440a2d39c8c6ec1f7
kimachinskiy <48508661+kimachinskiy@users.noreply.github.com>
2023-09-10 23:37:13 +0300

    Update settings.py

--- forum/settings.py
+++ forum/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', '178.212.110.52']
 
 # Application definition
 
