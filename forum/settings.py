1a8f2d04dde7f436f673e284bfcfdc025c331791
kimachinkiy <48508661+kimachinskiy@users.noreply.github.com>
2023-09-20 23:43:26 +0300

    Update settings

--- forum/settings.py
+++ forum/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', '178.212.110.52']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
 # Application definition
 
