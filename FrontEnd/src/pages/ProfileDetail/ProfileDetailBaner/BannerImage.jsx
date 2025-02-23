ef2ea4545a53b59b93c0a844b597143d553dfb05
Leonid Chelakhov <leonidchelahov@gmail.com>
2025-02-23 22:58:56 +0300

    Changed Tooltip, added cropped path in Profile detail

--- FrontEnd/src/pages/ProfileDetail/ProfileDetailBaner/BannerImage.jsx
+++ FrontEnd/src/pages/ProfileDetail/ProfileDetailBaner/BannerImage.jsx
@@ -3,8 +3,8 @@ import classes from './BannerImage.module.css';
 import PendingStatus from '../../../components/MiniComponents/PendingModerationIcon/PendingStatus';
 
 function BannerImage({ data }) {
-    const backgroundImage = data?.banner?.path
-        ? `url(${data.banner.path}) lightgray center / cover no-repeat`
+    const backgroundImage = data?.banner?.cropped_path
+        ? `url(${data.banner.cropped_path}) lightgray center / cover no-repeat`
         : `url(${process.env.REACT_APP_PUBLIC_URL}/svg/empty-decore-50x46.svg) #e0e0e0 left top / 50px 46px repeat `;
 
     return (
